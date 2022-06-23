ae0bf7fc326f573fed2599947c9255c81a923fe5
Egor011297 <89478128+Egor011297@users.noreply.github.com>
2022-06-23 16:05:27 +0300

    finished slider blog

--- js/main.js
+++ js/main.js
@@ -4,5 +4,7 @@ var mixer = mixitup('.portfolio__content');
 $(function() {
     $(".slider-block__inner").slick({
         dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt=""></button>'
     });
 });
